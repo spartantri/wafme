53814f29a158b9d91c6bd9895564e35f341656ed
spartantri <spartantri@users.noreply.github.com>
2017-10-05 02:19:12 +0200

    extractor function added

--- WAFme.py
+++ WAFme.py
@@ -11,7 +11,7 @@
 # Licence:     Apache2
 # -------------------------------------------------------------------------------
 
-import json
+import json, re
 
 def find_values(id, json_repr):
     results = []
@@ -23,3 +23,24 @@ def find_values(id, json_repr):
 
     json.loads(json_repr, object_hook=_decode_dict)  # Return value ignored.
     return results
+
+
+def extractor(jsonlog):
+    parsed=[]
+    for log in WAFme.find_values('messages', jsonlog):
+        var=re.search('\[data "Matched Data:.*found within (\S+): ', log[0])
+        id=re.search('\[id "[^"]+"]', log[0])
+        uri=re.search('^\w+ /\S+\?? HTTP/', WAFme.find_values('request_line', jsonlog))
+        txid=WAFme.find_values('transaction_id', jsonlog)
+        parsed.append([id, var, uri, txid])
+        print id, var, uri, txid
+    return
+
+def main():
+    soup = RuleEditor.get_ref_manual()
+    variables = RuleEditor.get_ref_section('Variables', soup)
+    t=tail.Tail('audit.log')
+    t.register_callback(extractor)
+    t.follow()
+    
+    
