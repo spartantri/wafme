16925cbb902e0653b52e5733e488324c2307f55f
spartantri <spartantri@users.noreply.github.com>
2017-10-05 15:13:45 +0200

    test

--- WAFme.py
+++ WAFme.py
@@ -39,11 +39,11 @@ def extractor(jsonlog):
             line=' '.join([line, var.group(1)])
         else:
             line=' '.join([line, log[0]])
-        uri=re.search('^\w+\s(\/[^\?\s]+)\??.*\sHTTP\/(?:(?:1|2)\.?(?:1|0)?)$', find_values('request_line', log[0]))
+        uri=re.search('^\w+\s(\/[^\?\s]+)\??.*\sHTTP\/(?:(?:1|2)\.?(?:1|0)?)$', find_values('request_line', log))
         if uri:
             line=' '.join([line, log[0]])
         else:
-            line=' '.join([line, find_values('request_line', log[0])])
+            line=' '.join([line, find_values('request_line', log)])
         txid=find_values('transaction_id', log[0])
         line=' '.join([line, txid[0]])
         print line
