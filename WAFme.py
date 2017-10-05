e245c709b0081728bb4e68795c9f327e03189baa
spartantri <spartantri@users.noreply.github.com>
2017-10-05 15:39:02 +0200

    debug

--- WAFme.py
+++ WAFme.py
@@ -29,6 +29,7 @@ def find_values(id, json_repr):
 def extractor(jsonlog):
     line=''
     for log in find_values('messages', jsonlog):
+        print type(log), log
         uri=re.search('^\w+\s(\/[^\?\s]+)\??.*\sHTTP\/(?:(?:1|2)\.?(?:1|0)?)$', find_values('request_line', log[0]))
         for event in log:
             if uri:
