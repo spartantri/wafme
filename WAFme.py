b023a0778ab386a62c38d565ffeb63679ea654f8
spartantri <spartantri@users.noreply.github.com>
2017-10-23 01:55:56 +0200

    regex search fix

--- WAFme.py
+++ WAFme.py
@@ -224,7 +224,7 @@ def add_sample(id, uri, var, content):
   #local_port=transaction["local_port"]
   sample='import requests\n'
   for header_key in headers.keys():
-      host_header=re.search("?i:(host)", header_key)
+      host_header=re.search("(?i)(host)", header_key)
       if host_header:
           host_header_name=host_header.group(1)
   if uri_check:
